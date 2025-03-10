efd741b63df8d17bc184c1aa1e6dd379f003cda4
Lim David <limdavid.663@gmail.com>
2025-03-10 08:07:53 +0700

    feat: update welcome page navigation to show dashboard link only for super admin users

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -11,13 +11,13 @@ import { Head, Link } from '@inertiajs/vue3';
         <header class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
             <nav class="flex items-center justify-end gap-4">
                 <Link
-                    v-if="$page.props.auth.user"
+                    v-if="$page.props.auth.user && $page.props.auth.user.is_super_admin"
                     :href="route('dashboard')"
                     class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                 >
                     Dashboard
                 </Link>
-                <template v-else>
+                <template v-else-if="!$page.props.auth.user">
                     <Link
                         :href="route('login')"
                         class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
